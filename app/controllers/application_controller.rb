8f9fb8d4c8299630e603ac4023330599654a9d58
heidless <lockhart.r@gmail.com>
2025-04-06 08:42:08 +0100

    organization_id in place

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :set_tenancy_organization
   before_action :configure_permitted_parameter_fields, if: :devise_controller?
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
 
-
   def stripe_payment; end
 
   protected
 
+  def set_tenancy_organization
+    @organization = 1
+    @org_name = Organization.find(@organization).name
+  end
+
   def configure_permitted_parameter_fields
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :avatar, :username, images: [] ])    # new
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :avatar, :username, images: [] ])   # edit
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :organization_id, :avatar, :username, images: [] ])    # new
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :organization_id, :avatar, :username, images: [] ])   # edit
   end
 end
